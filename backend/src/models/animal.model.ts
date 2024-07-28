210deea2fb71ce20cdf764f615c325320a46ed06
Luis Barrantes <luiyibarrantes@gmail.com>
2024-07-28 00:28:49 -0300

    update: -login responde rol y token -register con mensaje de registro correcto -getAll de post.service con info del refugio -getById de refugio.service con info de publicaciones y animales -animal.model agrego raza tamanio sexo y estadoSalud -updateUserValidator

--- backend/src/models/animal.model.ts
+++ backend/src/models/animal.model.ts
@@ -7,11 +7,15 @@ export interface Animal extends Document {
 	image: string;
 	estado: string;
 	refugio: ObjectId;
+	raza: string;
+	tamanio: string;
+	sexo: string;
+	estadoSalud: string;
 }
 
 export type createAnimalRequest = Pick<
 	Animal,
-	'nombre' | 'especie' | 'edad' | 'peso' | 'estado'
+	'nombre' | 'especie' | 'edad' | 'peso' | 'estado' | 'raza' | 'tamanio' | 'sexo' | 'estadoSalud'
 > & {
 	image: Express.Multer.File;
 };
@@ -49,7 +53,26 @@ const animalSchema = new mongoose.Schema<Animal>(
 			type: mongoose.Schema.Types.ObjectId,
 			ref: 'Refugio',
 			required: true
-		}
+		},
+		raza: {
+			type: String
+		},
+		tamanio: {
+			type: String,
+			enum: ['Chico', 'Mediano', 'Grande'],
+			default: 'Mediano'
+		},
+		sexo: {
+			type: String,
+			enum: ['Macho', 'Hembra'],
+			default: 'Macho'
+		},
+		estadoSalud: [
+			{
+				type: String,
+				enum: ['Sano', 'Enfermo', 'Vulnerable', 'Castrado', 'Desparasitado', 'Otro'],
+				default: 'Buena'
+			}]
 	},
 	{ timestamps: true }
 );
